346f0d02303c705161f94b19c04bfc859e130e91
sevil-ozdemir <sevil.tr.ozdemir@gmail.com>
2026-01-10 12:03:19 +0300

    Last updates

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,45 @@
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
+import { useDispatch } from 'react-redux';
+import { likeTwit, unlikeTwit } from '../twitsSlice.js';
+import axios from '../utils/axios.js';
 
 export default function Post({ post }) {
 
+  const dispatch = useDispatch();
   const date = dayjs(post.createDate).fromNow();
+
+  const handleLike = (e) => {
+
+    e.preventDefault();
+
+    axios
+      .post(`/twits/${post.id}/like`)
+      .then(resp => {
+
+        dispatch(likeTwit({ id: post.id }));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  const handleUnlike = (e) => {
+
+    e.preventDefault();
+
+    axios
+      .delete(`/twits/${post.id}/like`)
+      .then(resp => {
+        dispatch(unlikeTwit({ id: post.id }));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  const iconClass = post.likedByUser ? "bi bi-suit-heart-fill cursor-pointer hover:text-primary text-primary" : "bi bi-suit-heart cursor-pointer hover:text-primary text-gray-400";
 
   return <div className="flex flex-row container mx-auto bg-white w-[40vw] rounded-xl shadow-xl p-4 gap-6">
     <div className="flex flex-col justify-start w-24">
@@ -20,7 +55,7 @@ export default function Post({ post }) {
       </div>
       <div className="text-gray-400 text-sm">{date}</div>
       <div className="flex flex-row gap-2">
-        <i className="bi bi-suit-heart cursor-pointer hover:text-primary text-gray-400"></i> <span className="text-gray-400">{post.likes}</span> &nbsp;
+        <a href="#" onClick={post.likedByUser ? handleUnlike : handleLike}><i className={iconClass}></i> <span className="text-gray-400">{post.likes}</span> &nbsp;</a>
         <i className="bi bi-arrow-repeat cursor-pointer hover:text-primary text-gray-400"></i> <span className="text-gray-400">{post.retweets}</span> &nbsp;
         <i className="bi bi-chat cursor-pointer hover:text-primary text-gray-400"></i> <span className="text-gray-400">{post.replies}</span> &nbsp;
       </div>
