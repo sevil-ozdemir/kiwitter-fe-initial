0b1cae315c24f4e031840e00bdcdef5f292b9d8e
sevil-ozdemir <sevil.tr.ozdemir@gmail.com>
2026-01-13 20:22:02 +0300

    asd

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,82 +1,80 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useParams } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { selectTwitsByUsername, loadTwits } from "../twitsSlice.js";
-import { selectUser } from "../userSlice.js";
-import { toast } from "react-toastify";
+import { useSelector, useDispatch } from 'react-redux';
+import { selectTwitsByUsername } from '../twitsSlice.js';
+import { selectUser } from '../userSlice.js';
+import { loadTwits } from '../twitsSlice.js';
+import { toast } from 'react-toastify';
 
 import PageLayout from "../layouts/PageLayout.jsx";
 import Timeline from "../components/Timeline.jsx";
+
 import axios from "../utils/axios.js";
 
 export default function Profile() {
-  const { nick } = useParams(); // senin yapında nick vardı, onu koruduk
+
+  const { username } = useParams();
   const [userInfo, setUserInfo] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
-
   const dispatch = useDispatch();
+
   const user = useSelector(selectUser);
 
-  const isCurrentUser = user?.username === nick || user?.nickname === nick;
+  const isCurrentUser = user?.nickname === username;
 
-  // Kullanıcı bilgisi yükleme
   useEffect(() => {
+
     axios
-      .get(`/users/${isCurrentUser ? "me" : nick}`)
-      .then((res) => {
-        setUserInfo(res.data);
+      .get(`/users/${isCurrentUser ? "me" : username}`)
+      .then(response => {
+        setUserInfo(response.data);
       })
-      .catch((err) => {
-        console.error("Kullanıcı bilgisi alınamadı:", err);
+      .catch(error => {
+        console.error(error);
       });
-  }, [nick, isCurrentUser]);
 
-  // Twitleri yükleme
+  }, [username]);
+
   useEffect(() => {
+
     setIsLoading(true);
-    axios
-      .get("/twits")
-      .then((res) => {
-        dispatch(loadTwits(res.data.twits));
+
+    axios.get("/twits")
+      .then(response => {
+
+        dispatch(loadTwits(response.data.twits));
+
         setIsSuccess(true);
       })
-      .catch((err) => {
-        console.error("Twitler alınamadı:", err);
-        toast.error("Twitler yüklenirken hata oluştu");
+      .catch(error => {
+        console.error(error);
+
+        toast.error("Bir hata oluştu");
+
         setIsSuccess(false);
       })
-      .finally(() => setIsLoading(false));
-  }, [dispatch]);
+      .finally(() => {
 
-  const twits = useSelector(selectTwitsByUsername(nick));
+        setIsLoading(false);
+      });
+
+  }, []);
+
+  const twits = useSelector(selectTwitsByUsername(username));
 
   if (!userInfo) {
-    return (
-      <div className="text-center text-2xl font-bold text-gray-700">
-        Yükleniyor...
-      </div>
-    );
+    return <div className="text-center text-2xl font-bold text-white">Yükleniyor...</div>;
   }
 
-  return (
-    <PageLayout className="">
-      <div className="flex flex-col container mx-auto w-[60vw] p-4 gap-6 items-center">
-        <img
-          src={`https://i.pravatar.cc/1200?u=${userInfo.id}`}
-          alt={userInfo.name}
-          className="w-48 rounded-full aspect-square cursor-pointer"
-        />
-        <div className="text-center">
-          <h1 className="text-2xl font-bold text-gray-800 pb-0">
-            {userInfo.name}
-          </h1>
-          <span className="text-sm text-gray-500 italic">
-            ({userInfo.username || nick})
-          </span>
-        </div>
-        <Timeline posts={twits} isLoading={isLoading} isSuccess={isSuccess} />
+  return <PageLayout className="">
+    <div className="flex flex-col container mx-auto w-[60vw] p-4 gap-6 items-center">
+      <img src={`https://i.pravatar.cc/1200?u=${userInfo.id}`} alt={userInfo.name} className="w-48 rounded-full aspect-square cursor-pointer" />
+      <div className="text-center">
+        <h1 className="text-2xl font-bold text-white pb-0">{userInfo.name}</h1>
+        <span className="text-sm text-gray-100 italic">({userInfo.username})</span>
       </div>
-    </PageLayout>
-  );
+      <Timeline posts={twits} isLoading={isLoading} isSuccess={isSuccess} />
+    </div>
+  </PageLayout>;
 }
