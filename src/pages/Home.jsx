6765c80772c94db9e056376d56317b3b3a7fbc4a
sevil-ozdemir <sevil.tr.ozdemir@gmail.com>
2026-01-13 18:19:26 +0300

    postedit update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,63 +1,58 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { loadTwits, addTwit, selectTwits } from "../twitsSlice.js";
-import { selectUser } from "../userSlice.js";
-import { toast } from "react-toastify";
-
-import axios from "../utils/axios.js";
-import Timeline from "../components/Timeline.jsx";
+import { useSelector, useDispatch } from "react-redux";
+import { selectTwits, addTwit } from "../twitsSlice.js";
+import { useState } from "react";
 import PageLayout from "../layouts/PageLayout.jsx";
+import Post from "../components/Post.jsx";
 import PostEditor from "../components/PostEditor.jsx";
-import TimelineSelector from "../components/TimelineSelector.jsx";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isSuccess, setIsSuccess] = useState(false);
-  const [timelineMode, setTimelineMode] = useState("normal"); // normal, most_liked
-
   const dispatch = useDispatch();
-  const posts = useSelector(selectTwits(timelineMode));
-  const user = useSelector(selectUser);
-
-  const isLoggedIn = !!user?.token; // kullanıcı login kontrolü
-
-  useEffect(() => {
-    setIsLoading(true);
-
-    axios
-      .get("/twits")
-      .then((res) => {
-        dispatch(loadTwits(res.data.twits));
-        setIsSuccess(true);
-      })
-      .catch((err) => {
-        console.error(err);
-        toast.error("Twitler yüklenirken hata oluştu");
-        setIsSuccess(false);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [dispatch]);
+  const twits = useSelector(selectTwits("normal"));
+  const [mode, setMode] = useState("normal");
 
   const handleAddPost = (content) => {
-    axios
-      .post("/twits", { content })
-      .then((res) => {
-        dispatch(addTwit(res.data.twit));
-      })
-      .catch(() => toast.error("Twit eklenemedi"));
-  };
-
-  const handleTimelineModeChange = (mode) => {
-    setTimelineMode(mode);
+    const newTwit = {
+      id: crypto.randomUUID(),
+      authorId: 1000,
+      retweets: 0,
+      content,
+      createDate: Date.now(),
+      likes: 0,
+      replies: [],
+      name: "Sevil Ozdemir",
+      username: "sozdemir",
+      likedByUser: false,
+    };
+    dispatch(addTwit(newTwit));
   };
 
   return (
-    <PageLayout className="container mx-auto w-[40vw] py-8">
-      {isLoggedIn && <PostEditor addPost={handleAddPost} />}
-      <TimelineSelector mode={timelineMode} setMode={handleTimelineModeChange} />
-      <Timeline posts={posts} isLoading={isLoading} isSuccess={isSuccess} />
+    <PageLayout>
+      <div className="flex flex-col items-center gap-6 py-6">
+        <PostEditor addPost={handleAddPost} />
+        <div className="flex gap-4">
+          <button
+            className={`px-4 py-2 rounded ${
+              mode === "normal" ? "bg-lime-700 text-white" : "bg-gray-200"
+            }`}
+            onClick={() => setMode("normal")}
+          >
+            Normal
+          </button>
+          <button
+            className={`px-4 py-2 rounded ${
+              mode === "most-liked" ? "bg-lime-700 text-white" : "bg-gray-200"
+            }`}
+            onClick={() => setMode("most-liked")}
+          >
+            En Beğenilenler
+          </button>
+        </div>
+        {twits.map((twit) => (
+          <Post key={twit.id} post={twit} />
+        ))}
+      </div>
     </PageLayout>
   );
 }
+  
